5523aef47a31655c9566dfc27c95fc94662bff34
Alex Sandro de Almeida <sandergost@gmail.com>
2018-07-31 01:11:54 -0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<!--https://www.youtube.com/watch?v=bPZjuV4EdmE&index=1&list=PL6u82dzQtlfsV_n9w_wuhPulddNosaQru-->
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+        <meta name="description" content="Aqui vai uma descrição do website">
+        <meta name="author" content="Sahil Kumar">
+        <meta name="keywords" content="website, design">
+        
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        
+        <!--    STYLE CSS-->
+        <link rel="stylesheet" href="css/style.css">
+        
+        
+        <link rel="icon" href="images/fav.png">
+        <title>Sytle | Great Food and Wine</title>
+    <title>Document</title>
+</head>
+<body class="bg-dark">
+   <div class="container">
+       <div class="row p-2">
+           <div class="col-lg-4 offset-lg-4 bg-light text-dark mt-4 p-3 rounded">
+               <h3 class="text-center p-2">Insert Data Into Database</h3>
+               <form action="" method="post">
+                  <div class="form-group">
+                      <label for="name">Name</label>
+                      <input type="text" name="name" class="form-control" placeholder="Name" required>
+                  </div>
+                   
+               </form>
+           </div>
+       </div>
+   </div>
+    
+</body>
+</html>
